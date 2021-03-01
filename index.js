5847cbe6eeb73ccde30f0b67621b5ada17496012
TimTrayler <TimTrayler@users.noreply.github.com>
2021-03-01 20:21:41 +0100

    Programmed the base

---
+++ index.js
@@ -0,0 +1,56 @@
+
+var modules = [];
+
+
+if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+      return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+    };
+}
+
+
+function loadModules()
+{
+    var x = new XMLHttpRequest();
+
+    x.onreadystatechange = function()
+    {
+        const out = document.getElementById("output");
+
+        if(x.readyState == x.DONE)
+        {
+            if(x.status > 199 && x.status < 300)
+            {
+                x.responseText.split("\n").forEach(module =>{
+                    modules.push(module.trim());
+                });
+            }else {
+                out.innerHTML = "An error occurred!";
+                out.setAttribute("error", "");
+                check = () => {};
+            }
+        }
+    }
+
+    x.open("GET", "modules.txt");
+    x.send();
+}
+
+loadModules();
+
+function check(input)
+{
+    const module = input.value.trim().toLowerCase();
+    const out = document.getElementById("output");
+
+    if(modules.includes(module))
+    {
+        out.innerHTML = "'" + module + "' is a built-in module!";
+        out.setAttribute("success", "");
+        out.removeAttribute("error");
+    }else {
+        out.innerHTML = "'" + module + "' is not a built-in module!";
+        out.setAttribute("error", "");
+        out.removeAttribute("success");
+    }
+}
