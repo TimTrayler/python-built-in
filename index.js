e2cb7e0bfc2451df83f378a030d1d5ec0ff757cc
TimTrayler <67274738+TimTrayler@users.noreply.github.com>
2021-03-04 12:50:31 +0100

    Added Share

--- index.js
+++ index.js
@@ -4,30 +4,41 @@ var modules = [];
 
 if (!String.prototype.trim) {
     String.prototype.trim = function () {
-      return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+        return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
     };
 }
 
 
-function loadModules()
-{
+function share() {
+    if (navigator.share) {
+        navigator.share({
+
+        }).then(() => {
+            alert("Thanks for sharing!");
+        }).catch(() => {
+            alert("Please try again!");
+        });
+    } else {
+        document.getElementById("twitter-feedback").click();
+    }
+}
+
+
+function loadModules() {
     var x = new XMLHttpRequest();
 
-    x.onreadystatechange = function()
-    {
+    x.onreadystatechange = function () {
         const out = document.getElementById("output");
 
-        if(x.readyState == x.DONE)
-        {
-            if(x.status > 199 && x.status < 300)
-            {
-                x.responseText.split("\n").forEach(module =>{
+        if (x.readyState == x.DONE) {
+            if (x.status > 199 && x.status < 300) {
+                x.responseText.split("\n").forEach(module => {
                     modules.push(module.trim());
                 });
-            }else {
+            } else {
                 out.innerHTML = "An error occurred!";
                 out.setAttribute("error", "");
-                check = () => {};
+                check = () => { };
             }
         }
     }
@@ -38,17 +49,15 @@ function loadModules()
 
 loadModules();
 
-function check(input)
-{
+function check(input) {
     const module = input.value.trim().toLowerCase();
     const out = document.getElementById("output");
 
-    if(modules.includes(module))
-    {
+    if (modules.includes(module)) {
         out.innerHTML = "'" + module + "' is a built-in module!";
         out.setAttribute("success", "");
         out.removeAttribute("error");
-    }else {
+    } else {
         out.innerHTML = "'" + module + "' is not a built-in module!";
         out.setAttribute("error", "");
         out.removeAttribute("success");
